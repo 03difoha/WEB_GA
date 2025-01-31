6d2c4bbd7aec345c6f8697f7857051e82d017f73
03difoha <hdifolco@yahoo.com>
2025-01-31 18:00:06 +0000

    initial push

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import Grid from './components/Grid.vue'
 import { ref } from "vue";
-
+import { generatePopulation } from "./utils/GA.js";
 const gridSize = 16
 var pattern = ref([]);
 
@@ -12,6 +12,8 @@ function updatePattern(){
      pattern.value.push(Math.round(Math.random()))
   }
 }
+generatePopulation()
+
 </script>
 
 <template>
---
+++ src/utils/GA.js
@@ -0,0 +1,26 @@
+const generations = 100;
+const populationSize = 100;
+const mutationRate = 0.01;
+const genotypeLength = 5 ** 2
+// crossoverRate = 0.7;
+// elitism = 2;
+// tournamentSize = 5;
+
+export function generatePopulation() {
+  let population = [];
+  for (let i = 0; i < populationSize; i++) {
+    population.push(generateRandomIndividual());
+  }
+  return population;
+}
+
+function generateRandomIndividual() {
+  let individual = [];
+  for (let i = 0; i < genotypeLength; i++) {
+    individual.push(Math.random() < 0.5 ? 0 : 1);
+  }
+  return individual;
+}
+
+
+// console.log(generateInitialPopulation());
